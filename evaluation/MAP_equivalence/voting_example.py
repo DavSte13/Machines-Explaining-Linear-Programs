fa24e34de554f7aff4320f2e717a0f1b6bce34bc
David <david-steinmann@web.de>
2022-05-23 08:37:00 +0200

    evaluation renaming and added ilps

--- evaluation/MAP_equivalence/voting_example.py
+++ evaluation/MAP_equivalence/voting_example.py
@@ -5,15 +5,17 @@ import torch.nn as nn
 from cvxpylayers.torch import CvxpyLayer
 
 
-class MAP_LP(nn.Module):
+class MapLp(nn.Module):
     """
-    Torch module for the cvxpylayers emulation of the basic linear problem.
-    The objective function can later on be handed over as a parameter to the constructor.
-    reduce_dimensions can be used for granger causal attributions (reduces the number of constraints by 1)
-    m can be used to specify the number of constraints (if not given, it is assumed to be the same as n).
+    Torch module for the MAP voting example. The LP emulates a small graph with three nodes. Each node has its
+    individual preference to vote either for 1 or 0. Two nodes onnected by edges are more likely to vote for the
+    same result.
+
+    :param target: The target function, can either be the objective function 'cost' or the optimal solution 'opt'.
+    :param reduce_edge: If True, a smaller model is created where one edge is removed from the graph.
     """
 
-    def __init__(self, objective='cost', reduce_edge=False):
+    def __init__(self, target='cost', reduce_edge=False):
         super().__init__()
 
         if reduce_edge:
@@ -58,19 +60,19 @@ class MAP_LP(nn.Module):
 
         self.cvxpylayer = CvxpyLayer(prob, parameters=[theta], variables=[mu])
 
-        self.objective = objective
+        self.target = target
 
     def forward(self, theta):
         """
-        :param theta: value for the parameter A (as torch tensor)
-        :return: The objective function applied to the optimal solution of the LP
+        :param theta: value for the parameter A (as torch tensor).
+        :return: The target function of the LP.
         """
 
         solution,  = self.cvxpylayer.forward(theta)
 
-        if self.objective == 'opt':
+        if self.target == 'opt':
             result = solution
-        elif self.objective == 'cost':
+        elif self.target == 'cost':
             result = solution.mul(theta).sum()
         else:
             raise ValueError("Unknown objective function")
@@ -78,12 +80,13 @@ class MAP_LP(nn.Module):
         return result
 
 
-map_cost = MAP_LP('cost')
-map_opt = MAP_LP('opt')
+map_cost = MapLp('cost')
+map_opt = MapLp('opt')
 theta = torch.tensor([1., 0.3, 0.9, 0.9, 0.7, 1., 1.3, 0., 0., 1.3, 1.3, 0., 0., 1.3], requires_grad=True)
+# as both edges have the same values in theta, removing either of them results in the same theta_masked.
 theta_masked = torch.tensor([1., 0.3, 0.9, 0.9, 0.7, 1., 1.3, 0., 0., 1.3], requires_grad=True)
 
-map_cost_reduced = MAP_LP('opt', True)
+map_cost_reduced = MapLp('opt', True)
 masked_res = map_cost_reduced.forward(theta_masked)
 print(masked_res)
 
---
+++ problems/sp_ilp.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+from models.comboptnet import CombOptNetModule
+import numpy as np
+
+
+class ShortestPath(nn.Module):
+    """
+    Torch module for the cvxpylayers emulation of the shortest path problem.
+    The objective function can be handed over as a parameter to the constructor.
+
+    objective:    find the shortest path from source to target
+
+    :param target: The target function, can either be the objective function 'cost' or the optimal solution 'opt'.
+    """
+
+    def __init__(self, target='cost'):
+        super().__init__()
+
+        self.ilp_module = CombOptNetModule(var_ranges={'lb': 0, 'ub': 1}, tau=0.5)
+
+        self.target = target
+
+    def forward(self, A, b, c):
+        """
+        :param A: Incidence matrix for all nodes.
+        :param b: Vector of zeros with a one at the source and a -1 at the target indices.
+        :param c: Vector of all edge weights.
+        :return: Target function applied to the optimal solution of the LP.
+        """
+        c = c.view(1, -1)
+        constraints_1 = torch.hstack((A, b))
+        constraints_2 = -constraints_1
+        constraints = torch.vstack((constraints_1, constraints_2))
+
+        solution = self.ilp_module.forward(c, constraints)
+
+        if self.target == 'opt':
+            result = solution
+        elif self.target == 'cost':
+            result = solution.mul(c).sum()
+        elif self.target == 'sum':
+            result = solution.sum()
+        elif self.target == 'l2':
+            result = solution.dist(torch.tensor(np.zeros(self.m)))
+        else:
+            raise ValueError("Unknown objective function")
+
+        return result
